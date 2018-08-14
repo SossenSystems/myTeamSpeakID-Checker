4cd2f965dddca3cedb719e91c16646e0eca663a7
SossenSystems <33610430+SossenSystems@users.noreply.github.com>
2018-08-14 03:10:27 +0200

    Update myTeamSpeak.php

--- myTeamSpeak.php
+++ myTeamSpeak.php
@@ -18,7 +18,7 @@ function getEvent($event, $host){
 }
 // This function kicks user from the Server and write them a private message
 function kickUser($clientInfo, $host){
-    global $personaldmsg;
+    global $colors, $personaldmsg;
     // Selects client from Client ID
     $client = $host->serverGetSelected()->clientGetById($clientInfo["clid"]);
     // Send a private message on kick
@@ -27,12 +27,13 @@ function kickUser($clientInfo, $host){
     $client->kick('5', $personaldmsg['kickMsg']);
 
     $strOnKickToConsole = str_replace("%nickname%", $clientInfo['client_nickname'], $personaldmsg['kickConsole']);
-
-    echo (isset($strOnKickToConsole) && !empty($strOnKickToConsole) ? $strOnKickToConsole . "\n" : 'User ' . $clientInfo['client_nickname'] . ' kicked!');
+    echo $colors->getColoredString( "[INFO] ". (isset($strOnKickToConsole) && !empty($strOnKickToConsole) ? $strOnKickToConsole . "\n" : 'User ' . $clientInfo['client_nickname'] . ' kicked!' . "\n"), "white");
 }
 // This function is the heart of this script :D
 function runChecker($clientInfo, $host) {
-    global $personaldmsg, $kickoption;
+    global $colors, $personaldmsg, $kickoption, $option;
+    $client = $host->serverGetSelected()->clientGetById($clientInfo["clid"]);
+    $clientInfo = $client->getInfo();
     if($clientInfo != false) {
         if(isset($clientInfo['client_version']) && isset($kickoption['ClientMinVersion']) && !empty($kickoption['ClientMinVersion']) && $clientInfo['client_version'] >= $kickoption['ClientMinVersion']) {
             $startex = explode('Build: ', $clientInfo['client_version']);
@@ -43,23 +44,65 @@ function runChecker($clientInfo, $host) {
                     $match = preg_match("/^A[\da-zA-Z\/]{43}$/", $clientInfo['client_myteamspeak_id']);
                     if ($match) {
                         $strNicknameOnSuc = str_replace("%nickname%", $clientInfo['client_nickname'], $personaldmsg['successfulyJoinC']);
-                        echo $strNicknameOnSuc . "\n";
+                        echo $colors->getColoredString("[INFO] " . $strNicknameOnSuc, "green"), PHP_EOL;
                     } else {
                         kickUser($clientInfo, $host);
                     }
                 } else {
                     $strNicknameOnMod = str_replace("%nickname%", $clientInfo['client_nickname'], $personaldmsg['modified_myID']);
-                    echo $strNicknameOnMod . "\n";
+                    echo $colors->getColoredString("[SPOOF/MODIFIED] " . $strNicknameOnMod, "light_red"), PHP_EOL;
                     kickUser($clientInfo, $host);
                 }
             } else {
                 kickUser($clientInfo, $host);
             }
+            if($option['antiBadges-active'] == true || 1) {
+                if (isset($clientInfo['client_badges'])) {
+                    if (preg_match("(([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}))", $clientInfo['client_badges'])) {
+                        echo $colors->getColoredString("[INFO] User \"" . $clientInfo["client_nickname"] . "\" has set badges..."), PHP_EOL;
+                        echo $colors->getColoredString("[INFO] User \"" . $clientInfo["client_nickname"] . "\" kicked from the server, Reason: Valid badges..."), PHP_EOL;
+                        if (!isset($_SESSION['countOfKickedBadgesUser'])) {
+                            $_SESSION['countOfKickedBadgesUser'] = 1;
+                        } else {
+                            $_SESSION['countOfKickedBadgesUser']++;
+                        }
+                    }
+                }
+            }
         } else {
             $strNicknameOnVer = str_replace("%nickname%", $clientInfo['client_nickname'], $kickoption['ClientMinVersionElMsg']);
-            echo $strNicknameOnVer . "\n";
+            echo $colors->getColoredString("[WARNING] " . $strNicknameOnVer, "light_red"), PHP_EOL;
         }
     } else {
-        echo $personaldmsg['invalidclientid'] . "\n";
+        echo $colors->getColoredString("[NOTE] " . $personaldmsg['invalidclientid'], "light_blue"), PHP_EOL;
+    }
+}
+function isCommand($command, $eventText, $client){
+    $lenth = strlen($command);
+    $command_sub = substr($eventText, 0, $lenth);
+    if($command_sub == $command){
+        return true;
+    }
+}
+
+function convert($size, $unit)
+{
+    if ($unit == "KB") {
+        return $fileSize = round($size / 1024, 4) . ' KB';
+    }
+    if ($unit == "MB") {
+        return $fileSize = round($size / 1024 / 1024, 4) . ' MB';
+    }
+    if ($unit == "GB") {
+        return $fileSize = round($size / 1024 / 1024 / 1024, 4) . ' GB';
+    }
+}
+
+function isUID($uid){
+    global $settings;
+    foreach ($settings['masterUID'] as $uids){
+        if($uid == $uids){
+            return true;
+        }
     }
 }
